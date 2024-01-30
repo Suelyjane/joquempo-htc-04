bf25c5c44acdc2208e64bd4f48d08ef15e9dc530
Suelyjane <101297574+Suelyjane@users.noreply.github.com>
2024-01-29 21:34:41 -0300

    Iniciando JS no jogo

---
+++ index.js
@@ -0,0 +1,8 @@
+const $buttonStonePlayer1 = document.querySelector('.button-move-stone-1')
+const $moveBox1 = document.querySelector(.move-box-1)
+
+function handleStone1Move(){
+    alert('clicou na pedra 1')
+}
+
+$buttonStonePlayer1.addEventListener('click', handleStone1Move)
