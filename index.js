6aabfe1c6c1733645a55e680b2eecc45e5020a4d
Suelyjane <101297574+Suelyjane@users.noreply.github.com>
2024-01-31 21:55:30 -0300

    Adiciionado responsividade

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const $buttonStonePlayer1 = document.querySelector('.button-move-stone-1')
-const $moveBox1 = document.querySelector(.move-box-1)
+const $buttonStonePlayer2 = document.querySelector('.button-move-stone-2')
+const $moveBox1 = document.querySelector('#move-box-1')
+const $moveBox2 = document.querySelector('#move-box-2')
 
 function handleStone1Move(){
-    alert('clicou na pedra 1')
+    $moveBox1.innerHTML = 'img src=""'
 }
 
+function handlePaper1Move(){
+    $moveBox1.innerHTML = 'img src=""'
+}
+
+function handleScissors1Move(){
+    $moveBox1.innerHTML = 'img src=""'
+}
+
+function handleStone2Move(){
+    $moveBox1.innerHTML = 'img src=""'
+}
+
+function handlePaper2Move(){
+    $moveBox1.innerHTML = 'img src=""'
+}
+
+function handleScissors2Move(){
+    $moveBox1.innerHTML = 'img src=""'
+}
+
+$buttonStonePlayer1.addEventListener('click', handleStone1Move)
 $buttonStonePlayer1.addEventListener('click', handleStone1Move)
